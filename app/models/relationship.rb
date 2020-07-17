7dd734f308186112af9fb3b53b9f159c58245c4b
rayray <banapa2008@yahoo.co.jp>
2020-07-17 04:58:09 +0000

    Add user following

---
+++ app/models/relationship.rb
@@ -0,0 +1,8 @@
+class Relationship < ApplicationRecord
+  # 1対1の関連付け
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,14 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  
-  resources :account_activations, only: [:edit]
+  resources :users do                       # usersリソースをRESTfullな構造にするためのコード。
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations, only: [:edit]    # editアクションのみaccount_activationsリソースを適用
   resources :users
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
+  resources :password_resets,     only: [:new, :create, :edit, :update]     # password再設定用のリソースを適用
+  resources :microposts,          only: [:create, :destroy]     # micropostsリソースをcreateとdestroyアクションにのみ適用
+  resources :relationships,       only: [:create, :destroy]
 end
